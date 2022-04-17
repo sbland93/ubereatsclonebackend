09323ea4335423a296b475095fa0bb49c7eeeca2
Kim Seung Beom <rltmqj@gmail.com>
2022-04-17 19:56:11 +0900

    configService 추가 joi로 유효성 검증 typeORM 세팅 .env 분리

--- README.md
+++ README.md
@@ -37,3 +37,35 @@ export class createRestaurantDto {
 InputType 내부의 값을 하나하나 분리된 형태로 받아들이고 싶다며
 ArgsType을 활용.
 
+### TypeORM
+-- 개인적으로 예전 프로젝트에서 sequelize ORM을 썼었는데
+typescript기반으로 만들어진 TypeORM을 사용하면 좋을 것 같음.
+sequelize는 node.js를 지원하지만, typeORM은 좀 더 멀티플랫폼.
+
+
+
+### configService
+
+`
+typeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "sbland93",
+      password: "test",
+      database: "nubereats",
+      synchronize: true, //typeorm연결시에 migration
+      logging: true,
+    }),
+`
+
+dotenv 를 사용한 모듈임.
+
+`
+npm i corss-env 
+`
+// environment linux, macos, unix 등등 알아서 맞춰주도록.
+
+
+### 참고
+package.json 변경시에 재시작 필요.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi'; // js나 nestjs로 되어있지 않은 패키지를 import 하는 방법.
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 어디서든 접근 가능하도록 할 것인지
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod'), //유효성 검증.
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      host:process.env.DB_HOST,
+      port: +process.env.DB_PORT, //+"123123" string으로 변경
+      username:process.env.DB_USERNAME,
+      password:process.env.DB_PASSWORD,
+      database:process.env.DB_NAME,
+      type: "postgres",
+      synchronize: true, //typeorm연결시에 migration
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,                                     // 메모리에 생성
